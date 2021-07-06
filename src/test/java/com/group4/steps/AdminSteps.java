b4913a8da3400bd5eb273e03d97dac1dbde016bb
Boston Bragg <bostonbragg@gmail.com>
2021-07-05 23:01:48 -0500

    Merge issues

--- src/test/java/com/group4/steps/AdminSteps.java
+++ src/test/java/com/group4/steps/AdminSteps.java
@@ -1,6 +1,6 @@
 package com.group4.steps;
 
-import com.group4.runners.BasicRunnerM;
+import com.group4.runners.BasicRunner;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -22,17 +22,17 @@ public class AdminSteps {
         //String loginPagePath = "https://2105batch-project2-group4.s3.us-east-2.amazonaws.com/index.html";
         String elementID = "tenantList";
 
-        BasicRunnerM.driver.get(loginPagePath);
-        BasicRunnerM.loginPage.email.sendKeys("admin@BlissfulIsland.com");
-        BasicRunnerM.loginPage.password.sendKeys("admin");
-        BasicRunnerM.loginPage.loginButton.click();
-        WebDriverWait wait = new WebDriverWait(BasicRunnerM.driver, 2);
-        BasicRunnerM.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS) ;
+        BasicRunner.driver.get(loginPagePath);
+        BasicRunner.loginPage.email.sendKeys("admin@BlissfulIsland.com");
+        BasicRunner.loginPage.password.sendKeys("admin");
+        BasicRunner.loginPage.loginButton.click();
+        WebDriverWait wait = new WebDriverWait(BasicRunner.driver, 2);
+        BasicRunner.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS) ;
 
     }
     @When("The Admin clicks on the Account Maker button")
     public void the_Admin_clicks_on_the_Account_Maker_button() {
-        BasicRunnerM.messaging.accountMaker.click();
+        BasicRunner.messaging.accountMaker.click();
     }
     @Then("The Admin should be taken to the Create Account page")
     public void the_Admin_should_be_taken_to_the_Create_Account_page() {
@@ -41,70 +41,70 @@ public class AdminSteps {
     }
     @When("The Admin inputs the manager's first into the First Name field")
     public void the_Admin_inputs_the_manager_s_first_into_the_First_Name_field() {
-        BasicRunnerM.accountMaker.firstName.sendKeys("North");
+        BasicRunner.accountMaker.firstName.sendKeys("North");
     }
     @When("The Admin inputs the manager's last name into the Last Name field")
     public void the_Admin_inputs_the_manager_s_last_name_into_the_Last_Name_field() {
-        BasicRunnerM.accountMaker.lastName.sendKeys("South");
+        BasicRunner.accountMaker.lastName.sendKeys("South");
     }
     @When("The Admin inputs the manager's email address into the Email field")
     public void the_Admin_inputs_the_manager_s_email_address_into_the_Email_field() {
-        BasicRunnerM.accountMaker.email.sendKeys("north.south@blessfulisland.com");
+        BasicRunner.accountMaker.email.sendKeys("north.south@blessfulisland.com");
     }
     @When("The Admin inputs the password for the manager's account into the Password field")
     public void the_Admin_inputs_the_password_for_the_manager_s_account_into_the_Password_field() {
-        BasicRunnerM.accountMaker.password.sendKeys("password");
+        BasicRunner.accountMaker.password.sendKeys("password");
     }
     @When("The Admin inputs the manager's account number into the Account Type field")
     public void the_Admin_inputs_the_manager_s_account_number_into_the_Account_Type_field() {
-        BasicRunnerM.accountMaker.accountType().sendKeys("1");
+        BasicRunner.accountMaker.accountType().sendKeys("1");
 
     }
     @When("The Admin selects the manager's unit number from Unit ID dropdown menu")
     public void the_Admin_selects_the_manager_s_unit_number_from_Unit_ID_dropdown_menu() {
-        Select unitID = new Select(BasicRunnerM.driver.findElement(By.id("unitID")));
+        Select unitID = new Select(BasicRunner.driver.findElement(By.id("unitID")));
 
     }
     @When("The Admin clicks on the Submit button")
     public void the_Admin_clicks_on_the_Submit_button() {
-        BasicRunnerM.accountMaker.createNewAccountBtn.click();
+        BasicRunner.accountMaker.createNewAccountBtn.click();
     }
     @Then("The Admin should get a pop-up with a success message")
     public void the_Admin_should_get_a_pop_up_with_a_success_message() {
-        BasicRunnerM.driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS) ;
-        Assert.assertTrue(BasicRunnerM.accountMaker.alertMessageActiveM().getAttribute("class").contains("bg-success"));
+        BasicRunner.driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS) ;
+        Assert.assertTrue(BasicRunner.accountMaker.alertMessageActiveM().getAttribute("class").contains("bg-success"));
 
     }
     @Then("The Admin should be taken back to the admin portal")
     public void the_Admin_should_be_taken_back_to_the_admin_portal() {
-        BasicRunnerM.accountMaker.adminPortalButton.click();
+        BasicRunner.accountMaker.adminPortalButton.click();
     }
     // end of first scenario
 
     @When("The Admin clicks on the Tenant List plus sign")
     public void the_Admin_clicks_on_the_Tenant_List_plus_sign() throws InterruptedException {
         TimeUnit.SECONDS.sleep(5);
-        BasicRunnerM.adminPortal.plusBox.click();
+        BasicRunner.adminPortal.plusBox.click();
     }
     @Then("The Admin should see the accounts table")
     public void the_Admin_should_see_the_accounts_table() {
-        WebDriverWait wait1 = new WebDriverWait(BasicRunnerM.driver,2);
+        WebDriverWait wait1 = new WebDriverWait(BasicRunner.driver,2);
         wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"tenantListTable\"]")));
-        BasicRunnerM.adminPortal.tenantListTable.isDisplayed();
+        BasicRunner.adminPortal.tenantListTable.isDisplayed();
     }
     @When("The Admin clicks on the Make Manager button")
     public void the_Admin_clicks_on_the_Make_Manager_button() throws InterruptedException {
-        BasicRunnerM.adminPortal.promoteManager().click();
+        BasicRunner.adminPortal.promoteManager().click();
         TimeUnit.SECONDS.sleep(1);
     }
     @Then("A pop up asking for confirmation appears")
     public void a_pop_up_asking_for_confirmation_appears() {
-        BasicRunnerM.adminPortal.popUpContentHeader.isDisplayed();
+        BasicRunner.adminPortal.popUpContentHeader.isDisplayed();
     }
     @When("The Admin clicks on the pop up Make Manager button")
     public void the_Admin_clicks_the_Pop_up_Make_Manager_button() throws InterruptedException {
         TimeUnit.SECONDS.sleep(1);
-        BasicRunnerM.adminPortal.makeManager.click();
+        BasicRunner.adminPortal.makeManager.click();
     }
     @Then("The connected account should be promoted to a manager account")
     public void the_connected_account_should_be_promoted_to_a_manager_account() {
--- src/test/java/com/group4/steps/ManagerSteps.java
+++ src/test/java/com/group4/steps/ManagerSteps.java
@@ -30,7 +30,7 @@ public class ManagerSteps {
 
     @When("The Manager clicks on the Account Maker button")
     public void the_Manager_clicks_on_the_Tenant_Account_Management_button() {
-        BasicRunner.messagingCarlos.accountMakerButton.click();
+        BasicRunner.messaging.accountMakerButton.click();
     }
 
     @Then("The Manager should be redirected to the Create Account Page")
@@ -41,55 +41,55 @@ public class ManagerSteps {
     }
     @When("The Manager inputs the tenant's first name")
     public void the_Manager_inputs_the_tenant_s_first_name() {
-        BasicRunner.accountMakerCarlos.firstName.sendKeys("TestFirst");
+        BasicRunner.accountMaker.firstName.sendKeys("TestFirst");
     }
     @When("The Manager inputs the tenant's last name")
     public void the_Manager_inputs_the_tenant_s_last_name() {
-        BasicRunner.accountMakerCarlos.lastName.sendKeys("TestLast");
+        BasicRunner.accountMaker.lastName.sendKeys("TestLast");
     }
     @When("The Manager inputs the tenant's email address")
     public void the_Manager_inputs_the_tenant_s_email_address() {
-        BasicRunner.accountMakerCarlos.email.sendKeys("abc@google.com");
+        BasicRunner.accountMaker.email.sendKeys("abc@google.com");
     }
     @When("The Manager inputs the tenant's password")
     public void the_Manager_inputs_the_tenant_s_password() {
 
-        BasicRunner.accountMakerCarlos.password.sendKeys("password");
+        BasicRunner.accountMaker.password.sendKeys("password");
     }
     @When("The Manager inputs the tenant's unit number")
     public void the_Manager_inputs_the_tenant_s_unit_number() {
-        Select options = new Select(BasicRunner.accountMakerCarlos.selectUnits());
+        Select options = new Select(BasicRunner.accountMaker.selectUnits());
         options.selectByIndex(1);
     }
 
     @When("The Manager clicks the Submit button")
     public void the_Manager_clicks_the_Submit_button() {
-        BasicRunner.accountMakerCarlos.createNewAccountBtn.click();
+        BasicRunner.accountMaker.createNewAccountBtn.click();
     }
 
     @Then("The Manager should see a success message")
     public void the_Manager_should_see_a_success_message() {
-        Assert.assertTrue(BasicRunner.accountMakerCarlos.alertMessageActice().getAttribute("class").contains("bg-success"));
+        Assert.assertTrue(BasicRunner.accountMaker.alertMessageActice().getAttribute("class").contains("bg-success"));
     }
 
     @Then("The Submit button should be disabled")
     public void the_Submit_button_should_be_disabled() {
-        Assert.assertEquals(BasicRunner.accountMakerCarlos.createNewAccountBtn.getAttribute("disabled"), "true");
+        Assert.assertEquals(BasicRunner.accountMaker.createNewAccountBtn.getAttribute("disabled"), "true");
     }
 
     @Then("The Submit button should be enabled")
     public void the_Submit_button_should_be_enabled() {
-        Assert.assertNull(BasicRunner.accountMakerCarlos.createNewAccountBtnReenabled().getAttribute("enabled"));
+        Assert.assertNull(BasicRunner.accountMaker.createNewAccountBtnReenabled().getAttribute("enabled"));
     }
 
     @Then("The success message should disappear")
     public void the_success_message_should_disappear() {
-        Assert.assertTrue(BasicRunner.accountMakerCarlos.alertMessageInactive().getAttribute("class").contains("hidden"));
+        Assert.assertTrue(BasicRunner.accountMaker.alertMessageInactive().getAttribute("class").contains("hidden"));
     }
 
     @When("The Manager clicks on View Unit List")
     public void the_Manager_clicks_on_View_Unit_List() {
-        BasicRunner.messagingCarlos.unitListButton.click();
+        BasicRunner.messaging.unitListButton.click();
     }
 
     @Then("The Manager should be taken to the Unit List page")
@@ -109,9 +109,9 @@ public class ManagerSteps {
     @When("The manager clicks on the plus sign of Type {int} apartments")
     public void the_manager_clicks_on_the_plus_sign_of_Type_apartments(Integer int1) {
          if (int1 == 2) {
-            BasicRunner.unitListCarlos.normalPlus.click();
+            BasicRunner.unitList.normalPlus.click();
         }else if(int1 == 3){
-            BasicRunner.unitListCarlos.deluxPlus.click();
+            BasicRunner.unitList.deluxPlus.click();
         }
     }
 
@@ -119,10 +119,10 @@ public class ManagerSteps {
     public void the_manager_should_see_the_list_of_Type_apartments(Integer int1) throws InterruptedException {
         TimeUnit.SECONDS.sleep(2);
         if(int1 == 2){
-           WebElement col =  BasicRunner.unitListCarlos.normalLastRowCol1;
+           WebElement col =  BasicRunner.unitList.normalLastRowCol1;
            Assert.assertTrue(col.getAttribute("innerHTML").contains("210"));
         } else if (int1 == 3) {
-            WebElement col =  BasicRunner.unitListCarlos.deluxeLastRowCol1;
+            WebElement col =  BasicRunner.unitList.deluxeLastRowCol1;
             System.out.println(col.getAttribute("innerHTML"));
             Assert.assertTrue(col.getAttribute("innerHTML").contains("305"));
         }
@@ -131,10 +131,10 @@ public class ManagerSteps {
     @Then("The list of Type {int} apartments should be hidden")
     public void the_list_of_Type_apartments_should_be_hidden(Integer int1) {
         if (int1 == 2) {
-            WebElement table = BasicRunner.unitListCarlos.typeNormalTableClosed();
+            WebElement table = BasicRunner.unitList.typeNormalTableClosed();
             Assert.assertTrue(table.getSize().getHeight() < 200);
         }else if(int1 == 3){
-            WebElement table = BasicRunner.unitListCarlos.typeDeluxeTableClosed();
+            WebElement table = BasicRunner.unitList.typeDeluxeTableClosed();
             Assert.assertTrue(table.getSize().getHeight() < 200);
         }
     }
@@ -142,58 +142,58 @@ public class ManagerSteps {
     @Then("The manager should see a list of recent messages")
     public void the_manager_sees_recent_messages() throws InterruptedException{
         TimeUnit.SECONDS.sleep(2);
-        WebElement el =  BasicRunner.messagingCarlos.recentMessageRow1Col1;
+        WebElement el =  BasicRunner.messaging.recentMessageRow1Col1;
         Assert.assertNotNull(el.getAttribute("innerHTML"));
     }
 
     @When("The manager clicks on the plus sign")
     public void the_manager_clicks_on_the_plus_sign() throws InterruptedException{
             TimeUnit.SECONDS.sleep(2);
-            BasicRunner.messagingCarlos.plusBtn.click();
+            BasicRunner.messaging.plusBtn.click();
     }
 
     @Then("The manager should see a list of past messages")
     public void the_manager_should_see_a_list_of_past_messages() throws InterruptedException {
-        WebElement el =  BasicRunner.messagingCarlos.pastMessagesRow1Col1;
+        WebElement el =  BasicRunner.messaging.pastMessagesRow1Col1;
         Assert.assertNotNull(el.getAttribute("innerHTML"));
     }
 
     @When("The manager clicks on the respond button of a message")
     public void the_manager_clicks_on_the_respond_button_of_a_message() {
-        BasicRunner.messagingCarlos.respondBtnRow1().click();
+        BasicRunner.messaging.respondBtnRow1().click();
     }
 
     @Then("The manager should see a message popup")
     public void the_manager_should_see_a_message_popup() {
-        WebElement el = BasicRunner.messagingCarlos.messagePopUp;
+        WebElement el = BasicRunner.messaging.messagePopUp;
         Assert.assertTrue(el.getAttribute("style").contains("block"));
     }
     @When("The manager enters message into message input")
     public void the_manager_enters_message_into_message_input() {
-        BasicRunner.messagingCarlos.messageText.sendKeys("Selenium Sample Message");
+        BasicRunner.messaging.messageText.sendKeys("Selenium Sample Message");
     }
     @When("The manager clicks the submit message button")
     public void the_manager_clicks_the_submit_message_button() {
-        BasicRunner.messagingCarlos.responseSubmit.click();
+        BasicRunner.messaging.responseSubmit.click();
     }
     @Then("The submit message button should be disabled")
     public void the_submit_message_button_should_be_disabled() {
-        Assert.assertEquals(BasicRunner.messagingCarlos.responseSubmit.getAttribute("disabled"), "true");
+        Assert.assertEquals(BasicRunner.messaging.responseSubmit.getAttribute("disabled"), "true");
     }
     @Then("The popup should close")
     public void the_popup_should_close() {
-        WebElement popup = BasicRunner.messagingCarlos.messagePopUpHidden();
+        WebElement popup = BasicRunner.messaging.messagePopUpHidden();
         Assert.assertTrue(popup.getAttribute("style").contains("none"));
     }
 
     @Then("The new message should be displayed at the top of recent messages")
     public void the_new_message_should_be_displayed_at_the_top_of_recent_messages() {
-        String msg = BasicRunner.messagingCarlos.recentFirstRowMessage.getAttribute("innerHTML");
+        String msg = BasicRunner.messaging.recentFirstRowMessage.getAttribute("innerHTML");
         Assert.assertTrue(msg.equals("Selenium Sample Message"));
     }
 
     @When("The manager clicks on popup close button")
     public void the_manager_clicks_on_the_close_popup_btn() {
-        BasicRunner.messagingCarlos.closeBtn.click();
+        BasicRunner.messaging.closeBtn.click();
     }
 }
